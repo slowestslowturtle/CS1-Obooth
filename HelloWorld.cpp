4864ce4d2224c54be51468fd3ab9a765b2398d3d
Owen <obooth@mavs.coloradomesa.edu>
2024-01-25 09:39:51 -0700

    yep

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //I made some Notes Srew Capitalization
+    cout << "Hello World Again" << endl;
+    cout << "my name is Owen" << endl;
+    cout << "Goodbye" <<endl;
+    return 0;
+}
