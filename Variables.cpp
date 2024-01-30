12409659b8460b6696d42bcd5693ff3bb5800fc8
Owen <obooth@mavs.coloradomesa.edu>
2024-01-30 09:51:10 -0700

    start of variables

---
+++ Variables.cpp
@@ -0,0 +1,20 @@
+/*
+Name Owen booth
+
+All ABout Variables
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1 = 42;
+    
+    cout << number1 << endl;
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}
