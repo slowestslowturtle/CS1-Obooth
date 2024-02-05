4d98604ae1f28f23062f560709c02fae7c69e7da
Owen <obooth@mavs.coloradomesa.edu>
2024-02-05 09:11:35 -0700

    Stuff

--- Variables.cpp
+++ Variables.cpp
@@ -20,5 +20,34 @@ int main()
     long number2;
     cout << &number1 << endl;
     cout << &number2 << endl;
+    int number4;
+    int number5;
+
+    cout << "Please Enter Number 1 :";
+    cin >> number4;
+
+    cout << "please enter number 2 :";
+    cin >> number5;
+
+    cout << number4 << "+" << number5 << "=" << number4 + number5 << endl;
+    string myname;
+    cout << "what is your name? ";
+
+    // cin >> myname >> name2;
+    getline(cin, myname);
+    cout << "Welcome " << myname << " to our program." << endl;
+
+    string somephrase;
+    somephrase = "a\tb\n";
+    cout << somephrase;
+    somephrase = "aa\tb\n";
+    cout << somephrase;
+    somephrase = "aaa\tb\n";
+    cout << somephrase;
+    somephrase = "aaaa\tb\n";
+    cout << somephrase;
+    somephrase = "aaaaa\tb\n";
+    cout << somephrase;
+    somephrase = "aaaaaa\tb\n";
     return 0;
 }
