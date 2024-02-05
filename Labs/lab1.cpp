c7d6d22f3cd93def1180a855ed05194801946092
Owen <obooth@mavs.coloradomesa.edu>
2024-02-05 10:36:29 -0700

    done

--- Labs/lab1.cpp
+++ Labs/lab1.cpp
@@ -39,14 +39,14 @@ int main()
     cout << line1;
     line1 = "  / 0 \\   **********************\n";
     cout << line1;
-    //FIXME6: use variable to print the second line line of the graphic
-    //FIXME7: print the third line of the graphics
-    //FIXME8: use variable to print the fourth line
-    //FIXME9: use variable to print the fifth line
+    //FIXME6: use variable to print the second line line of the graphic #fixed
+    //FIXME7: print the third line of the graphics #fixed
+    //FIXME8: use variable to print the fourth line #fixed
+    //FIXME9: use variable to print the fifth line #fixed
     //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
 
     cout << "\nGood bye... hit enter to exit the program: " << '\n';
-    //FIXME10: make the console wait for user input;
+    //FIXME10: make the console wait for user input; #fixed
     getchar();
     return 0; //exit program by returning 0 status to the system
 }
