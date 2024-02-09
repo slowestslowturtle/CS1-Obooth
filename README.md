22ee431ac147f7d6f40b127ce45f8ea1f9d664e6
Owen <obooth@mavs.coloradomesa.edu>
2024-02-09 09:15:46 -0700

    Homework Final

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ I'm typing in split screen and not a new line
 
 Assingments
 
+Homework #1
+    Name: Std IO Homwework
+    Description: The Stages of Habgman
+    Due Date: 2-12-24
+    Status: Complete
+    Location: https://github.com/slowestslowturtle/CS1-Obooth/tree/main/Homework/Homework1
+    Self Grade: 100/100
+    Notes: Impossible hangman quite fun.
 
 Labs
 
--- Variables.cpp
+++ Variables.cpp
@@ -5,11 +5,24 @@ All ABout Variables
 */
 
 #include <iostream>
+#include <iomanip>
+
 
 using namespace std;
 
 int main()
 {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout << left << setw (10) << "name" << setw(10) << "Title" << setw(10) << "position" << endl;
+
+    cout << setfill('c');
+    cout << setw(48) << "" << endl;
+    cout << setfill(' ');
+
+    cout << setw(10) << "Jeremy" << setw(25) << "maser of the universe" << setw (13) << "All knowing" << endl;
+    
+    cout << setprecision(10) << 42.012345678901234567890 << endl;
+
     int number3 = 'B';
     
     cout << number3 << endl;
@@ -31,9 +44,10 @@ int main()
 
     cout << number4 << "+" << number5 << "=" << number4 + number5 << endl;
     string myname;
-    cout << "what is your name? ";
+    cout << "what is your name?\n ";
 
     // cin >> myname >> name2;
+    cin.get();
     getline(cin, myname);
     cout << "Welcome " << myname << " to our program." << endl;
 
@@ -49,5 +63,6 @@ int main()
     somephrase = "aaaaa\tb\n";
     cout << somephrase;
     somephrase = "aaaaaa\tb\n";
+    cout << somephrase;
     return 0;
 }
