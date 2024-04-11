a3b22f06cfccd94324e9d180df4df4d76298dc0d
Owen <obooth@mavs.coloradomesa.edu>
2024-04-11 08:55:36 -0600

    while test

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main()
 {
-    //I made Some notes Srew Capitalization
-    cout << "Hello World Again" << endl;
-    cout << "my name is Owen" << endl;
-    cout << "Goodbye" <<endl;
+    // I made Some notes Srew Capitalization
+    double num1 = 45, num2 = 1;
+    while (num1 != num2)
+    {
+        cout << "Hello World Again" << endl;
+        cout << "my name is Owen" << endl;
+        cin >> num2;
+    }
+    cout << "Goodbye" << endl;
     return 0;
 }
