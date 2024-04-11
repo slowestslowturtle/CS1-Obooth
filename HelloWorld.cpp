5f4879bf342de7c997d30fb619614620ade4d953
Owen <obooth@mavs.coloradomesa.edu>
2024-04-11 09:44:23 -0600

    for while loop

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+double guesscheck(double, double);
 
 using namespace std;
 
@@ -6,12 +7,42 @@ int main()
 {
     // I made Some notes Srew Capitalization
     double num1 = 45, num2 = 1;
+    int i = 0;
     while (num1 != num2)
     {
         cout << "Hello World Again" << endl;
         cout << "my name is Owen" << endl;
         cin >> num2;
+        if (guesscheck(num1, num2) == 0)
+        {
+            cout << "hiGh\n";
+        }
+        if (guesscheck(num1, num2) == 1)
+        {
+            cout << "lOw\n";
+        }
+        if (guesscheck(num1, num2) == 2)
+        {
+            cout << num2 <<'\n';
+        }
+        i++;
+        if (i==6)
+        {
+            break;
+        }
     }
     cout << "Goodbye" << endl;
     return 0;
+}
+double guesscheck(double num1, double num2)
+{
+    if (num1 < num2)
+    {
+        return 0;
+    }
+    if (num1 > num2)
+    {
+        return 1;
+    }
+    return 2;
 }
